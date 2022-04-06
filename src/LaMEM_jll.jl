3de171ee6c445945fe47520e17f0f620bc5c778e
jlbuild <juliabuildbot@gmail.com>
2022-04-06 14:38:12 +0000

    LaMEM_jll build 1.1.0+0

---
+++ src/LaMEM_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule LaMEM_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("LaMEM")
+JLLWrappers.@generate_main_file("LaMEM", UUID("15d6fa20-f789-5486-b71b-22b4ac8eb1c1"))
+end  # module LaMEM_jll
