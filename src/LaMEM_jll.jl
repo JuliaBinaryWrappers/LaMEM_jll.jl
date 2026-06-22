e7046a7366f7eb64b1f509a71bbb44464593ffde
jlbuild <juliabuildbot@gmail.com>
2026-06-22 18:03:16 +0000

    LaMEM_jll build 2.2.1+0

--- src/LaMEM_jll.jl
+++ src/LaMEM_jll.jl
@@ -8,5 +8,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("LaMEM")
-JLLWrappers.@generate_main_file("LaMEM", UUID("15d6fa20-f789-5486-b71b-22b4ac8eb1c1"))
+JLLWrappers.@generate_main_file("LaMEM", Base.UUID("15d6fa20-f789-5486-b71b-22b4ac8eb1c1"))
 end  # module LaMEM_jll
